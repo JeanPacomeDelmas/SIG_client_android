ce5bcd29205a0dda9ead0884019fab6917ef92ff
Arthur Pointel <arthur.pointel@etu.univ-orleans.fr>
2020-12-11 23:17:24 +0100

    Client

---
+++ app/src/main/java/com/example/mybat/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.mybat;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.google.fr");
+    }
+}
