095f2643fe8b6689afb6a8e97b158f81b044d4fc
Arthur Pointel <arthur.pointel@etu.univ-orleans.fr>
2020-12-16 18:27:01 +0100

    Essai

---
+++ app/src/main/java/com/sig/sig_app/LoginActivity.java
@@ -0,0 +1,109 @@
+package com.sig.sig_app;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private final String SPRINGSERV_URL = "http://192.168.1.38:8081/api/utilisateur/";
+    private String userLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+
+    public void buttonSeConnecter(View v) {
+        userLogin = ((TextView)findViewById(R.id.login)).getText().toString();
+        // Initialisation du login
+        new MyTask().execute();
+
+        Intent intent = new Intent(this, OpenlayersActivity.class);
+        intent.putExtra("login", userLogin);
+        startActivity(intent);
+    }
+
+    private class MyTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            URL url = null;
+            try {
+                url = new URL(SPRINGSERV_URL);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+
+            HttpURLConnection con = null;
+            try {
+                con = (HttpURLConnection)url.openConnection();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                con.setRequestMethod("POST");
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            }
+            con.setRequestProperty("Content-Type", "application/json; utf-8");
+            con.setRequestProperty("Accept", "application/json");
+            con.setRequestProperty("Access-Control-Allow-Origin", "*");
+            con.setRequestProperty("Access-Control-Allow-Headers", "*");
+            con.setRequestProperty("mode", "cors");
+            con.setDoOutput(true);
+            String jsonInputString = "{\"username\": \"" + userLogin + "\"}";
+            System.out.println(jsonInputString);
+
+            try {
+                try(OutputStream os = con.getOutputStream()) {
+                    byte[] input = jsonInputString.getBytes("utf-8");
+                    os.write(input, 0, input.length);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            int code = 0;
+            try {
+                code = con.getResponseCode();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println(code);
+
+            try {
+                try(BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"))){
+                    StringBuilder response = new StringBuilder();
+                    String responseLine = null;
+                    while ((responseLine = br.readLine()) != null) {
+                        response.append(responseLine.trim());
+                    }
+                    System.out.println(response.toString());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
+    }
+}
---
+++ app/src/main/java/com/sig/sig_app/OpenlayersActivity.java
@@ -0,0 +1,95 @@
+package com.sig.sig_app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.vision.barcode.Barcode;
+import com.notbytes.barcode_reader.BarcodeReaderActivity;
+
+public class OpenlayersActivity extends AppCompatActivity {
+
+    private final int BARCODE_READER_ACTIVITY_REQUEST = 1;
+    private final int ADD_QR_CODE_ACTIVITY_REQUEST = 2;
+    private final String OPENLAYERS_URL = "http://192.168.1.38:1234/";
+    private final String SPRINGSERV_URL = "http://192.168.1.38:8081/api/";
+
+    private String userLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        userLogin = getIntent().getStringExtra("login");
+
+        setContentView(R.layout.activity_openlayers);
+
+        // Initialisation de la web view qui contient le js d'Openlayers
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAllowFileAccessFromFileURLs(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+        webView.loadUrl(OPENLAYERS_URL);
+    }
+
+    public void buttonQRcode(View v) {
+        Intent launchIntent = BarcodeReaderActivity.getLaunchIntent(this, true, false);
+        startActivityForResult(launchIntent, BARCODE_READER_ACTIVITY_REQUEST);
+    }
+
+    public void addQRCode(View v) {
+        Intent launchIntent = BarcodeReaderActivity.getLaunchIntent(this, true, false);
+        startActivityForResult(launchIntent, ADD_QR_CODE_ACTIVITY_REQUEST);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode != Activity.RESULT_OK) {
+            Toast.makeText(this, "error in  scanning", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (requestCode == BARCODE_READER_ACTIVITY_REQUEST && data != null) {
+            Barcode barcode = data.getParcelableExtra(BarcodeReaderActivity.KEY_CAPTURED_BARCODE);
+            WebView webView = (WebView) findViewById(R.id.webview);
+            String dirBuilder = OPENLAYERS_URL + this.userLogin + "/" + barcode.rawValue;
+            webView.loadUrl(dirBuilder);
+            Toast.makeText(this, dirBuilder, Toast.LENGTH_SHORT).show();
+        }
+
+        if (requestCode == ADD_QR_CODE_ACTIVITY_REQUEST && data != null) {
+            Barcode barcode = data.getParcelableExtra(BarcodeReaderActivity.KEY_CAPTURED_BARCODE);
+            WebView webView = (WebView) findViewById(R.id.webview);
+            String dirBuilder = OPENLAYERS_URL + "addqrc/" + this.userLogin + "/" +  barcode.rawValue;
+            webView.loadUrl(dirBuilder);
+            Toast.makeText(this, dirBuilder, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
